b93dd2864c5b9cf3dff92d3c87724e2b40e6fb0c
Wagner Alcantara <wagner.alcantara@outlook.com.br>
2021-04-03 14:08:51 -0300

    Exercicio calcula area terreno

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		//Exercicio calcular area terreno
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double largura  = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = metroQuadrado * area;
+		
+		System.out.printf("Area = %.2f%n", area);
+		System.out.printf("Preco = %.2f%n", preco);
+		
+		sc.close();
+		
+		
 		/*
 		 *Exemplo de funções matematicas 
 		 */
 		//exemplo de função de matriz quadrada
-		double x = 3.0;
+		/*
+		 * double x = 3.0;
+		 
 		double y = 4.0;
 		double z = -5.0;
 		double A, B, C;
@@ -32,6 +53,7 @@ public class Main {
 		B = Math.abs(z);
 		System.out.println("O valor absoluto de "+y+ " é "+A);
 		System.out.println("O valor absoluto de "+z+ " é "+B);
+		
 		
 		/*
 		 * Exemplo de saida de dados
